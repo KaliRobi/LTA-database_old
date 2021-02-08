eb9eb00c1d584ddd8c92952d631ee7cdc768fb60
KaliRobi <66567828+KaliRobi@users.noreply.github.com>
2021-02-08 08:34:47 +0100

    Update lta_database.py

--- Lta with flask/flaskr/lta_database.py
+++ Lta with flask/flaskr/lta_database.py
@@ -2,38 +2,85 @@ import click
 from flask import current_app, g
 from flask.cli import with_appcontext
 import psycopg2
+from psycopg2 import pool
 
 
-
-# function to connect the database for data
-
-def get_dt_db():
-    if 'db' not in g:
-        g.db = psycopg2.connect(
-            current_app.config['RECORDS_DATABASE']
-        )
-        g.db.row_factory = psycopg2.row
-    
-    return g.db
 
 # function to connect the database for data for the users
 def get_users_db():
-    if 'db' not in g:
-        g.db = psycopg2.connect(
-            current_app.config['USERS_DATABASE']
-        )
-        g.db.row_factory = psycopg2.row
+    init_users_db = psycopg2.pool.SimpleConnectionPool(1,20,            
+            dbname = "lta_users",
+            user = "lta_user_mas",
+            host =  "localhost",
+            password="p")
+    if(init_users_db):
+        print('pool created')
+    conn = init_users_db.getconn()
+    if(conn):
+        print('conencted too')
+        c = conn.cursor()
+    return init_users_db
+    
 
-# start up the database, authenticate too 
-def start_dt_db():
-    db =  get_dt_db()
-    with current_app.open_resource('records.sql') as rec_dab:
-        db.executescript(rec_dab.read().decode('UTF8'))
+def get_dt_db():
+    data_pool = psycopg2.pool.ThreadedConnectionPool(1,20,
+                dbname = "lta_data",
+                user = "lta_data_mas",
+                password = "p",
+                host = "localhost")
+    if(data_pool):
+        print('data pool created')
+    conn = data_pool.getconn()
+    if(conn):
+        print('data connection')
+    return data_pool
 
-def start_users_db():
-    db = get_users_db()
-    with current_app.open_resource('users.sql') as users_dab:
-        db.executescript(users_dab.read().decode('UTF8'))
+
+def init_dt_db():
+    init_data_db = psycopg2.pool.ThreadedConnectionPool(1,20,
+                dbname = "lta_data",
+                user = "lta_data_mas",
+                password = "p",
+                host = "localhost")
+    conn = init_data_db.getconn()
+    with conn.cursor() as cursor:
+        dbquery = current_app.open_resource('records.sql').read()
+        
+        cursor.execute(dbquery)
+    conn.commit()
+        
+        
+            
+        
+def init_users_db():
+            init_users_db = psycopg2.pool.SimpleConnectionPool(1,20,            
+            dbname = "lta_users",
+            user = "lta_user_mas",
+            host =  "localhost",
+            password="p")
+            conn = init_users_db.getconn()   
+            with conn.cursor()  as cursor:
+                dbquery =  current_app.open_resource('users.sql').read()
+                print(dbquery)
+                cursor.execute(dbquery)
+            conn.commit()
+
+ # the table control which monitors who entered what when and with what operator (insert, update)                 
+def init_usrdt():
+    # init_usrdt = psycopg2.pool.SimpleConnectionPool(1,5,
+    #         dbname = "lta_ownership",
+    #         user = "lta_owners",
+    #         host =  "localhost",
+    #         password="proba123")
+    pass
+    
+    
+    
+    
+
+def get_userdt():
+    pass
+                  
 
 #closes the conenction
 def close_db(e=None):
@@ -44,10 +91,23 @@ def close_db(e=None):
 
 # cleans up after call
 
+@click.command('init-users-db')
+@with_appcontext
+def init_user_command():
+    """Clear the existsing data and create new tables"""
+    init_users_db()
+    click.echo('Initialized the user database')
+
+@click.command('init-dt-db')
+@with_appcontext
+def init_db_command():
+    """Clear the existsing data and create new tables"""
+    init_dt_db()
+    click.echo('Initialized the data database')
+
+
+
 def init_app(app):
     app.teardown_appcontext(close_db)
-    
-
-
-
-
+    app.cli.add_command(init_db_command)
+    app.cli.add_command(init_user_command)
