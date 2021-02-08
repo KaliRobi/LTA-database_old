b45dc32e0c01892ba49674ac4ae97caa62fc5a72
KaliRobi <66567828+KaliRobi@users.noreply.github.com>
2021-02-08 08:29:44 +0100

    Update ltausers.py

--- Lta with flask/flaskr/ltausers.py
+++ Lta with flask/flaskr/ltausers.py
@@ -1,7 +1,10 @@
-from flask import Flask, Blueprint, render_template, g, request, url_for, session
-import functools
+from flask import Flask, Blueprint, redirect, render_template, g, request, url_for, flash, session, current_app
 from werkzeug.security import check_password_hash, generate_password_hash
-from flaskr.lta_database import get_users_db
+from flaskr_p3.lta_database import get_users_db
+from flaskr_p3.db_queries import record_counter
+from psycopg2 import pool
+import functools
+
 
 
 bp = Blueprint('users', __name__, url_prefix='/users' )
@@ -9,26 +12,66 @@ bp = Blueprint('users', __name__, url_prefix='/users' )
 
 @bp.route('/login', methods=('GET', 'POST'))
 def login_to_app():
-    if request.method == 'POST':
+    record_num = record_counter()
+    if request.method == 'POST':        
         username = request.form['username']
         password = request.form['password']
-        db = get_users_db()
+        
+        conn = get_users_db().getconn()
+        cur = conn.cursor()
         error = None
-        user = get_users_db.execute(
-            "SELECT * FROM user WHERE username = ?", (username,)
-        ).fetchone()
+        cur.execute(
+            "SELECT * FROM lta_users WHERE username = %s", (username,)
+        )
+        user = cur.fetchone()
+        
 
         if user is None:
             error = 'Username or password is incorrect'
-        elif not check_password_hash(user['password'], password):
+        elif not user[2] == password:
+        # elif not check_password_hash(user['password'], password):  no self password set yet
             error = 'Username or password is incorrect'
         
         if error is None:
             session.clear()
-            session['user_id'] = user['Uiid']
-            return 'you are in'
-
-
-
+            session["current_user"] = user[1]           
+            
+            return render_template('input.html', records = record_num)
+        
+        flash(error)
 
     return render_template('forusers.html')
+
+
+@bp.before_app_request
+def user_session():
+    if "current_user" not in session:
+        g.user = None
+    else:
+        user = session["current_user"]
+        conn = get_users_db().getconn()
+        with conn.cursor() as cursor:
+            dbquery = current_app.open_resource('user_session.sql').read()
+            query_res = cursor.execute(dbquery, (user,))
+
+            conn.commit()
+
+        
+
+        
+        
+        
+def login_needed(view):
+        @functools.wraps(view)
+        def wrapped_view(**kwargs):
+            if session["current_user"] is None:
+                return redirect(url_for('users.login_to_app'))
+            
+            return view(**kwargs)
+        
+        return wrapped_view
+
+@bp.route('/logoff')
+def logoff():
+    session.clear()
+    return redirect(url_for('users.login_to_app'))
